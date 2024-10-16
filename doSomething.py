271778fc32400ed1c676fd2c8899bf4f3d48dc90
tejasmitra <73795182+tejasmitra@users.noreply.github.com>
2024-10-16 17:38:41 -0400

    Adding the action in our repository

---
+++ doSomething.py
@@ -0,0 +1,5 @@
+name = input("What is your name")
+favorite_class = input("What is your favorite class")
+school = input("Where do you go to school")
+
+print("This is  " + name + " and they go to " + school + ". Their favorite class is " + favorite_class + ".")
